32fac7a1373c13d3058d77cdbefc5f6dbab5ef93
Dan Ziemba <zman0900@gmail.com>
2012-09-27 05:58:10 -0400

    Added logging and fixed scripts

--- zip/run.sh
+++ zip/run.sh
@@ -1,18 +1,28 @@
 #!/tmp/busybox sh
 
-export PATH=/tmp:$PATH
+cd /tmp
+
+# remove old log
+rm -rf /sdcard/kernel_update.log
+# everything is logged into /sdcard/bml_over_mtd.log
+exec >> /sdcard/kernel_update.log 2>&1
+set -x
+
+export PATH=/tmp:/:/sbin:/system/xbin:/system/bin:$PATH
+
 # Set up busybox symlinks
 for i in $(busybox --list)
 do
 	ln -s busybox /tmp/$i
 done
 
-./dump_image boot old.img
-eval $(grep -m 1 -A 1 BOOT_IMAGE_OFFSETS old.img | tail -n 1)
-./replace_kernel old.img $boot_offset $boot_len $recovery_len zImage new.img
+bml_over_mtd dump boot 72 reservoir 4012 old.img
+eval $(busybox grep -m 1 -A 1 BOOT_IMAGE_OFFSETS old.img | busybox tail -n 1)
+replace_kernel old.img $boot_offset $boot_len $recovery_len zImage new.img
 if [ "$?" -eq '0' ]; then
-./erase_image boot
-./bml_over_mtd.sh boot 72 reservoir 4012 ./new.img
+erase_image boot
+bml_over_mtd flash boot 72 reservoir 4012 new.img
+eval $(grep -m 1 -A 1 BOOT_IMAGE_OFFSETS new.img | tail -n 1)
 else
 exit 1
 fi
